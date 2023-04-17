d9a677b2019ed04bc74d97b5baed6c6ee6d2a345
Elisabetta Manca <elisabetta.manca@physics.ucla.edu>
2023-04-17 11:57:37 -0700

    fixing issue with ordering of syst df

--- templateMaker/templates_with_pandas.py
+++ templateMaker/templates_with_pandas.py
@@ -106,8 +106,7 @@ f = h5py.File("templatesTest2.hdf5","r")
 results = narf.ioutils.pickle_load_h5py(f["results"])
 
 Hdata_obs = results['dataPostVFP']["output"]["data_obs"].get()
-# lumi = results['dataPostVFP']["lumi"]
-lumi = 16.8
+lumi = results['dataPostVFP']["lumi"]
 xsec = results['ZmumuPostVFP']["dataset"]["xsec"]
 
 H = results['ZmumuPostVFP']['output']['signalTemplates_nominal'].get() #boost histogram values
@@ -117,11 +116,12 @@ H = H*lumi*1000*xsec/results["ZmumuPostVFP"]["weight_sum"]
 t = h5py.File('templatesFit.hdf5','r') # templates file used for getting the cross sections (later will also be boost histogram)
 H3 = results['ZmumuPostVFP']['output']['signalTemplates_mass'].get() #boost histogram with systematic mass variations
 H3 = H3*lumi*1000*xsec/results["ZmumuPostVFP"]["weight_sum"]
-
-M = H3[:,:,:,:,:,:,[bh.loc('massShift100MeVDown') , bh.loc('massShift100MeVUp')]] #boost histogram for selected mass variations
+M = H3[:,:,:,:,:,:,[bh.loc('massShift50MeVDown') , bh.loc('massShift50MeVUp')]] #boost histogram for selected mass variations
 low_acc      = results['ZmumuPostVFP']['output']['lowacc'].get().to_numpy()[0].reshape(-1,2)
+low_acc = low_acc*lumi*1000*xsec/results["ZmumuPostVFP"]["weight_sum"]
 low_acc_mass = results['ZmumuPostVFP']['output']['lowacc_mass'].get()
-low_acc_mass_array = low_acc_mass[:,:,:,[bh.loc('massShift100MeVDown') , bh.loc('massShift100MeVUp')]].to_numpy()[0].reshape(-1,2,2)
+low_acc_mass = low_acc_mass*lumi*1000*xsec/results["ZmumuPostVFP"]["weight_sum"]
+low_acc_mass_array = low_acc_mass[:,:,:,[bh.loc('massShift50MeVDown') , bh.loc('massShift50MeVUp')]].to_numpy()[0].reshape(-1,2,2)
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Building the nominal dataframe~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 '''Unpacking the data'''
@@ -144,8 +144,7 @@ multi = pd.MultiIndex.from_product(iterables , names = ['rapidity', 'qt' , 'hel'
 
 #building dataframe
 df = pd.DataFrame(a , index = multi)
-
-
+print(df.head())
 '''Adding cross section information to our dataframe by creating cross section dataframe and merging'''
 qtBins = np.array([0., 3., 6., 9.62315204,12.36966732,16.01207711,21.35210602,29.50001253,60.,200.]) #these have to be like this for now
 yBins = np.array([0., 0.4, 0.8, 1.2, 1.6, 2.0, 2.4, 3.0, 10.0])
@@ -166,10 +165,9 @@ xsec_df['charge'] = charges
 #now the dataframe carries cross section column
 df = df.merge(xsec_df ,left_on=['rapidity','qt','hel','charge'], right_on=['rapidity','qt','hel','charge'])
 
-
 #setting process as index & cleaning up by removing redundant information
 df.set_index(['helXsec_'+df['hel']+'_y_'+df['rapidity'].apply(lambda x: round(x,1)).apply(str)+'_qt_'+df['qt'].apply(lambda x: round(x,1)).apply(str),df['charge']],inplace=True)
-df.drop(columns=['rapidity','qt','hel','charge'],inplace=True)
+df.drop(columns=['rapidity','qt','charge'],inplace=True)
 df.rename_axis(['process','charge'] ,inplace=True)
 
 #reorganizing data into a single column labeled 'data' - contains unrolled pt/eta distribution
@@ -182,18 +180,33 @@ df['isSignal']  = True
 print('Nominal Dataframe:')
 print(df.head())
 
-#low acceptance
-df.loc[('low_acc',-1.0),:] = [-1 , low_acc[:,0] ,np.nan, False]
-df.loc[('low_acc', 1.0),:] = [-1 , low_acc[:,1] ,np.nan, False]
+norm_chan = df.loc['helXsec_T_y_{}_qt_{}'.format(round(yBinsC[1],1),round(qtBinsC[5],1))]['data'].values[1]
 
-print(df.loc['low_acc'])
+# #low acceptance
+# df.loc[('low_acc',-1.0),:] = [-1 , low_acc[:,0] ,np.nan, False]
+# df.loc[('low_acc', 1.0),:] = [-1 , low_acc[:,1] ,np.nan, False]
+
+# print(df.loc['low_acc'])
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Building the dataframe for the mass variation systematics~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 
 #reshaping data 
-mass_arr = M.to_numpy()[0]
+good_idx_mass = [5,15]
+# print(H3.values()[0,0,...,0,good_idx_mass][H3.values()[0,0,...,0,good_idx_mass]!=0.0])
+
+mass_arr = H3.values()[...,good_idx_mass]
+# mass_arr = M.to_numpy()[0]
+# print(mass_arr[0,0,...,0,:][mass_arr[0,0,...,0,:]!=0.0],"corr")
+
 mass_unrolled = mass_arr.reshape((6,8,-1,2,6,2))                     #shape: (6,8,2880,2,6,2)
-mass_swapped = np.swapaxes(mass_unrolled , 2,-1).reshape(-1,2880)    #rapidity, qt, mass variation, charge,helicity 
+# rapidity, qt, data, charge, hel, syst
+# 'rapidity', 'qt' , 'hel','charge', syst, data
+# swapping data for mass
+# rapidity, qt, syst, charge, hel, data
+mass_swapped = np.swapaxes(mass_unrolled , 2,-1)
+# swapping mass for hel
+# rapidity, qt, hel, charge, syst, data
+mass_swapped = np.swapaxes(mass_swapped , 2,4).reshape(-1,2880)
 
 #Building the pandas dataframe indexed by these values
 m_yBinsC               = M.axes['Zrap'].centers
@@ -201,62 +214,108 @@ m_qtBinsC              = M.axes['Zpt'].centers
 m_charges              = M.axes['charge'].centers
 m_helicities      = list(M.axes['helicities'])
 mass_variations   = list(M.axes['massShift']) #only teo in this case
-
 #multi index object
-m_iterables = [m_yBinsC, m_qtBinsC,mass_variations,m_charges ,m_helicities] #order must match mass_swapped array shape
-m_multi = pd.MultiIndex.from_product(m_iterables , names = ['rapidity', 'qt' ,'syst' ,'charge','hel'])
+m_iterables = [m_yBinsC, m_qtBinsC,m_helicities,m_charges ,mass_variations] #order must match mass_swapped array shape
+m_multi = pd.MultiIndex.from_product(m_iterables , names = ['rapidity', 'qt' ,'hel' ,'charge','syst'])
 
 #building dataframe and setting index values as process strings
 m_df = pd.DataFrame(mass_swapped , index = m_multi)
+print(m_df.head())
 m_df.reset_index(inplace=True)
+print(m_df.head())
 m_df.set_index(['helXsec_'+m_df['hel']+'_y_'+m_df['rapidity'].apply(lambda x: round(x,1)).apply(str)+'_qt_'+m_df['qt'].apply(lambda x: round(x,1)).apply(str),m_df['charge'], m_df['syst']],inplace=True)
-m_df.drop(columns=['rapidity','qt','hel','charge','syst'],inplace=True)
+print(m_df.head())
+m_df.drop(columns=['rapidity','qt','charge','syst'],inplace=True)
+print(m_df.head())
 m_df.rename_axis(['process','charge','variation_index'] ,inplace=True)
+print(m_df.head())
 
 
 #reorganizing data into a single column labeled 'data' - contains unrolled pt/eta distribution
 m_df['data'] = m_df.loc[:,0:2879].apply(np.hstack , axis=1) 
 m_df.drop(columns = m_df.loc[:,0:2879].columns , inplace = True)
-
-#low acceptance
-m_df.loc[('low_acc',-1.0,'massShift100MeVDown')] = [low_acc_mass_array[:,0,0]]
-m_df.loc[('low_acc', 1.0,'massShift100MeVDown')] = [low_acc_mass_array[:,1,0]]
-m_df.loc[('low_acc',-1.0,'massShift100MeVUp')]   = [low_acc_mass_array[:,0,1]]
-m_df.loc[('low_acc', 1.0,'massShift100MeVUp')]   = [low_acc_mass_array[:,1,1]]
-print('\nadding low acceptance')
-print(m_df.loc['low_acc'])
+print(m_df.head())
+# #low acceptance
+# m_df.loc[('low_acc',-1.0,'massShift50MeVDown')] = [low_acc_mass_array[:,0,0]]
+# m_df.loc[('low_acc', 1.0,'massShift50MeVDown')] = [low_acc_mass_array[:,1,0]]
+# m_df.loc[('low_acc',-1.0,'massShift50MeVUp')]   = [low_acc_mass_array[:,0,1]]
+# m_df.loc[('low_acc', 1.0,'massShift50MeVUp')]   = [low_acc_mass_array[:,1,1]]
+# print('\nadding low acceptance')
+# print(m_df.loc['low_acc'])
 
 #logK
 m_df['syst']      = m_df.index.get_level_values(2).map(lambda x: x.split('V')[0] + 'V')
 m_df['variation'] = m_df.index.get_level_values(2).map(lambda x: x.split('V')[1])
+print(m_df.head())
+print(m_df.loc['helXsec_T_y_{}_qt_{}'.format(round(yBinsC[1],1),round(qtBinsC[5],1))].query("variation == 'Down'")['data'].values)
+data = m_df.loc['helXsec_T_y_{}_qt_{}'.format(round(yBinsC[1],1),round(qtBinsC[5],1))].query("variation == 'Down'")['data'].values[1]
+print(data[data!=0.0])
+
 m_df.reset_index(inplace=True)
+print(m_df.head())
 m_df.drop(columns = 'variation_index' , inplace = True)
 m_df.set_index(['process','charge'] , inplace =True)
 
-m_down = m_df.sort_index(level=['process','charge']).query("variation == 'Down'")['data']
-m_up   = m_df.sort_index(level=['process','charge']).query("variation == 'Up'")['data']
+m_down = m_df.query("variation == 'Down'")['data']
+m_up   = m_df.query("variation == 'Up'")['data']
+print(m_down.head())
+nominal= df['data']
 
-nominal= df.sort_index(level=['process','charge'])['data']
+logkepsilon = math.log(1e-3)
 
-logk_up   =(m_up  /nominal).apply(lambda x: np.nan_to_num( np.log(x) , nan=math.log(1e-3)))
-logk_down =(m_down/nominal).apply(lambda x: np.nan_to_num(-np.log(x) , nan=math.log(1e-3)))
+#m_down = m_df.set_index(['process','charge']).query("variation == 'Down'")['data']
+#m_up   = m_df.set_index(['process','charge']).query("variation == 'Up'")['data']
+
+#nominal= df.sort_index(level=['process','charge'])['data']
+
+logk_up   = (m_up/nominal).apply(lambda x: np.log(x))
+logk_down = (m_down/nominal).apply(lambda x: -np.log(x))
 
 
-m_df = pd.DataFrame({'logK':pd.concat([logk_up , logk_down]), 'variation':['Up']*578+['Down']*578}).reset_index()\
-.merge(m_df.reset_index() , left_on=['process','charge','variation'],right_on=['process','charge','variation'])\
-.drop(columns=['data'])
+#true if product is positive
+truth_up   = (nominal*m_up).apply(lambda x: np.equal(np.sign(x),1))
+truth_down = (nominal*m_down).apply(lambda x: np.equal(np.sign(x),1))
+
+
+epsilon_up   =   logk_up.apply(lambda x: logkepsilon*np.ones_like(x))
+epsilon_down = logk_down.apply(lambda x: -logkepsilon*np.ones_like(x))
+
+
+logk_up   = pd.Series([np.where(truth_up.values[i]\
+          ,(m_up/nominal).apply(lambda x: np.log(x)).values[i]\
+          , epsilon_up.values[i])\
+             for i in range(len(logk_up))] , index = logk_up.index)
+logk_down = pd.Series([np.where(truth_down.values[i]\
+          ,(m_down/nominal).apply(lambda x: -np.log(x)).values[i]\
+          , epsilon_down.values[i])\
+             for i in range(len(logk_up))] , index = logk_down.index)
+
+logk_up   = pd.DataFrame({'logK':logk_up , 'variation':'Up'})
+logk_down = pd.DataFrame({'logK':logk_down , 'variation':'Down'})
+
+m_df = pd.concat([logk_up,logk_down])
 
 print('\nSystematics Dataframe')
 print(m_df.head())
+
+# print(m_df.loc['helXsec_T_y_{}_qt_{}'.format(round(yBinsC[1],1),round(qtBinsC[5],1))].query("variation == 'Down'")['logK'].values[1])
+# data2 = m_df.loc['helXsec_T_y_{}_qt_{}'.format(round(yBinsC[1],1),round(qtBinsC[5],1))].query("variation == 'Down'")['logK'].values[1]
+# # print(norm_chan[norm_chan>0])
+# # data2 = np.where(np.equal(np.sign(norm_chan*data),1), data2, -logkepsilon*np.ones_like(data2))
+# print(data2[data2<6.9])
+
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
 
 #retrieve metadata
 
 procs = list(df.query("charge==1.0").index.get_level_values(0))
+procs2 = list(m_df.query("charge==1.0").index.get_level_values(0))
+for p1,p2 in zip(procs,procs2):
+    print(p1,p2)
 signals = list(df.query("charge==1.0 & isSignal==True").index.get_level_values(0))
-nproc = len(processes)
+nproc = len(procs)
 nsignals = len(signals)
-maskedchans = ['Wlike_minus','Wlike_plus']
+maskedchans = ['Wlike_minus']
 
 #list of groups of signal processes by charge - DON'T NEED THAT
 chargegroups = []
@@ -327,21 +386,21 @@ poly2dreggroupbincenters1 = []
 
 #list of systematic uncertainties (nuisances)
 systsd = OrderedDict()
-systs = []
+systs = ['mass']
 systsnoprofile = []
-systsnoconstraint = []
+systsnoconstraint = ['mass']
 
-for syst in systs:
-    if not 'NoProfile' in syst[2]:
-      systsd[syst[0]] = syst
-      systs.append(syst[0])
-for syst in systs:
-    if 'NoProfile' in syst[2]:
-      systsd[syst[0]] = syst
-      systs.append(syst[0])
-      systsnoprofile.append(syst[0])
-    if 'NoConstraint' in syst[2]:
-        systsnoconstraint.append(syst[0])
+# for syst in systs:
+#     if not 'NoProfile' in syst[2]:
+#       systsd[syst[0]] = syst
+#       systs.append(syst[0])
+# for syst in systs:
+#     if 'NoProfile' in syst[2]:
+#       systsd[syst[0]] = syst
+#       systs.append(syst[0])
+#       systsnoprofile.append(syst[0])
+#     if 'NoConstraint' in syst[2]:
+#         systsnoconstraint.append(syst[0])
 
 nsyst = len(systs)
   
@@ -512,12 +571,18 @@ constraintweights = None
 
 
 data_obs = np.concatenate((Hdata_obs.to_numpy()[0][...,0].ravel(),Hdata_obs.to_numpy()[0][...,1].ravel()))
+# data_obs = Hdata_obs.to_numpy()[0][...,1].ravel()
+
 
 nbytes += writeFlatInChunks(data_obs, f, "hdata_obs", maxChunkBytes = chunkSize)
 data_obs = None
 
-sumw = np.concatenate((H[sum,sum,:,:,0,sum].values().ravel(),H[sum,sum,:,:,0,sum].values().ravel()))
-sumw2 = np.concatenate((H[sum,sum,:,:,0,sum].variances().ravel(),H[sum,sum,:,:,0,sum].variances().ravel()))
+sumw = np.concatenate((H[sum,sum,:,:,0,sum].values().ravel(),H[sum,sum,:,:,1,sum].values().ravel()))
+sumw2 = np.concatenate((H[sum,sum,:,:,0,sum].variances().ravel(),H[sum,sum,:,:,1,sum].variances().ravel()))
+
+# sumw = H[sum,sum,:,:,0,sum].values().ravel()
+# sumw2 = H[sum,sum,:,:,0,sum].variances().ravel()
+
 
 #compute poisson parameter for Barlow-Beeston bin-by-bin statistical uncertainties
 kstat = np.square(sumw)/sumw2
@@ -533,11 +598,55 @@ sumw = None
 nbytes += writeFlatInChunks(sumw2, f, "hsumw2", maxChunkBytes = chunkSize)
 sumw2 = None
 
-norm = np.concatenate((np.stack(df.query("charge==-1.0")['data'].values,axis=-1),np.stack(df.query("charge==1.0")['data'].values,axis=-1),np.expand_dims(np.stack(df.query("charge==-1.0")['xsec'].values,axis=-1),axis=0),np.expand_dims(np.stack(df.query("charge==1.0")['xsec'].values,axis=-1),axis=0)),axis=0)
+norm = np.concatenate((np.stack(df.query("charge==-1.")['data'].values,axis=-1),np.stack(df.query("charge==1.")['data'].values,axis=-1),np.expand_dims(np.stack(df.query("charge==-1.")['xsec'].values,axis=-1),axis=0),np.expand_dims(np.stack(df.query("charge==1.")['xsec'].values,axis=-1),axis=0)),axis=0)
 nbytes += writeFlatInChunks(norm, f, "hnorm", maxChunkBytes = chunkSize)
 norm = None
 
-logk = np.zeros([5762,nproc,2,nsyst], dtype)
+# norm = np.concatenate((np.stack(df.query("charge==1.")['data'].values,axis=-1),np.expand_dims(np.stack(df.query("charge==1.")['xsec'].values,axis=-1),axis=0)),axis=0)
+# nbytes += writeFlatInChunks(norm, f, "hnorm", maxChunkBytes = chunkSize)
+
+# m_df = m_df[m_df.index.get_level_values(0).map(lambda x: x.find('UL') > 1 or x.find('L') > 1)]
+print("logk shape",m_df['logK'].values[0].shape)
+logk_ups = np.stack(m_df.query("charge==-1.0  & variation == 'Up'")['logK'].values,axis=-1)
+logk_downs = np.stack(m_df.query("charge==-1.0  & variation =='Down'")['logK'].values,axis=-1)
+
+logkavg = 0.5*(logk_ups + logk_downs)
+logkhalfdiff = 0.5*(logk_ups - logk_downs)
+
+# #ensure that systematic tensor is sparse where normalization matrix is sparse
+# logkavg = np.where(np.equal(norm,0.), 0., logkavg)
+# logkhalfdiff = np.where(np.equal(norm,0.), 0., logkhalfdiff)
+
+logk_minus = np.stack((logkavg,logkhalfdiff),axis=-1)
+
+#same for plus
+
+logk_ups = np.stack(m_df.query("charge==1.0 & variation == 'Up'")['logK'].values,axis=-1)
+logk_downs = np.stack(m_df.query("charge==1.0 & variation =='Down'")['logK'].values,axis=-1)
+
+logkavg = 0.5*(logk_ups + logk_downs)
+logkhalfdiff = 0.5*(logk_ups - logk_downs)
+
+# #ensure that systematic tensor is sparse where normalization matrix is sparse
+# logkavg = np.where(np.equal(norm,0.), 0., logkavg)
+# logkhalfdiff = np.where(np.equal(norm,0.), 0., logkhalfdiff)
+
+logk_plus = np.stack((logkavg,logkhalfdiff),axis=-1)
+
+print(logk_plus.shape,logk_minus.shape)
+
+logk = np.concatenate((logk_minus,logk_plus,np.zeros((1,nproc,2)),np.zeros((1,nproc,2))), axis=0)
+
+# logk = np.concatenate((logk_minus,np.zeros((1,nproc,2))), axis=0)
+
+# logk = np.zeros([5762,nproc,2,nsyst], dtype)
+# print(nproc)
+# logk[:,-1,:,0] = np.concatenate((logk_minus,logk_plus,np.zeros((1,289,2)),np.zeros((1,289,2))), axis=0)[:,-1,:]
+logk = np.expand_dims(logk,axis=-1) #placeholder
+
+# logk=np.nan_to_num(logk,0.0)
+
+print(logk.shape)
 nbytes += writeFlatInChunks(logk, f, "hlogk", maxChunkBytes = chunkSize)
 logk = None
 
