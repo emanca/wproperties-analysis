bee1cdf1d9a0a1b89929ed5128f7ad16f4548bba
Elisabetta Manca <elisabetta.manca@cern.ch>
2023-07-27 15:14:06 +0200

    script for submitting toys

--- Fit/runToys.py
+++ Fit/runToys.py
@@ -7,34 +7,31 @@ import numpy as np
 import argparse
 from multiprocessing import Pool, cpu_count
 
+def generate_seeds(seed, num_seeds):
+    seed_seq = np.random.SeedSequence(seed)
+    seeds = seed_seq.generate_state(num_seeds)
+    return seeds
+
+def runSingleToy(toy):
+    # combinetf = 'combinetf.py --nThreads=3 --bootstrapData -t25 --seed {}  --yieldProtectionCutoff 100. --allowNegativePOI  {}.pkl_sparse.hdf5 -o FitRes/fit_{}_toy{}.root'.format(seeds[toy], charge,charge, toy)
+    combinetf = 'combinetf.py --nThreads=3 -t-1 --seed {}  --yieldProtectionCutoff 100. --allowNegativePOI  ../templateMaker/bstrp_inputs/Wplus_bstrp_{}.hdf5 --saveHists --doJacobian -o FitRes/fit_Wplus_btsrp{}_asimov.root'.format(seeds[toy],toy,toy)
+    print('executing', combinetf)
+    os.system(combinetf)
+
 parser = argparse.ArgumentParser("")
 parser.add_argument('-t', '--t',type=int, default=1, help='number of toys to run')
 
 args = parser.parse_args()
-
 toys = args.t
-charge = "WPlus"
+
+seed = 260292
+n_bstrp = 100
+
+# seeds = generate_seeds(seed, n_bstrp)
 seeds = np.random.randint(100000, size=(toys))
 print(seeds)
-def runSingleToy(toy):
-    # combinetf = 'combinetf.py --nThreads=3 --bootstrapData -t25 --seed {}  --yieldProtectionCutoff 100. --allowNegativePOI  {}.pkl_sparse.hdf5 -o FitRes/fit_{}_toy{}.root'.format(seeds[toy], charge,charge, toy)
-    combinetf = 'combinetf.py --nThreads=3 -t40 --bootstrapData --seed {}  --yieldProtectionCutoff 100. --allowNegativePOI  {}.pkl_sparse.hdf5 -o FitRes/fit_{}_toy{}.root'.format(seeds[toy], charge,charge, toy)
-    print('executing', combinetf)
-    os.system(combinetf)
 
-f = fitUtils(doSyst=True,channels =["{}_preVFP".format(charge),"{}_postVFP".format(charge)])
-f.fillProcessList()
-f.shapeFile()
-f.maskedChannels()
-f.fillHelGroup()
-f.setPreconditionVec()
-f.fillSumGroup()
-f.fillHelMetaGroup()
-f.makeDatacard()    
-text2hd5f = 'text2hdf5_npinput.py --allowNegativeExpectation --sparse --maskedChan={}_preVFP_xsec --maskedChan={}_postVFP_xsec {}.pkl --out {}.pkl.root'.format(charge,charge,charge, charge)
-print('executing', text2hd5f) 
-os.system(text2hd5f)
 
-toy = [i for i in range(toys)]
+toy = [i for i in range(n_bstrp)]
 pool=Pool()
 pool.map(runSingleToy, toy)
